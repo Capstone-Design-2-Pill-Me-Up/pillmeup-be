4dadf631bfe2b4610950a268ffd1442f79ef271c
wonbj <kaiuo97@naver.com>
2025-10-12 01:29:16 +0900

    feat: jwt 세팅

---
+++ PillMeUp/src/main/java/com/capstone/pillmeup/global/exception/security/jwt/dto/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.capstone.pillmeup.global.exception.security.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+
+	private String accessToken;
+	
+}
