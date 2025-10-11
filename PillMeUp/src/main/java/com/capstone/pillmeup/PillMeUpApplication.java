dc4aef5159b6b94cffdc459c75d9890148aaa6a4
wonbj <kaiuo97@naver.com>
2025-10-12 01:23:38 +0900

    chore: 초기 개발 환경 설정

---
+++ PillMeUp/src/main/java/com/capstone/pillmeup/PillMeUpApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.pillmeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillMeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillMeUpApplication.class, args);
+	}
+
+}
---
+++ PillMeUp/src/test/java/com/capstone/pillmeup/PillMeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.pillmeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillMeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
