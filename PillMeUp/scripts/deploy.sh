ca77dd269845e1b7ef3e64b146e3e6775a85a260
mjkim <mjkim134@naver.com>
2025-10-14 13:25:31 +0900

    fix

--- PillMeUp/scripts/deploy.sh
+++ PillMeUp/scripts/deploy.sh
@@ -1,22 +1,28 @@
+#!/bin/bash
+
 REPOSITORY=/home/ubuntu/app
-cd $REPOSITORY
+PROJECT_NAME=pillmeup
 
-APP_NAME=demo
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "실행 중인 애플리케이션 PID: $CURRENT_PID"
 
-if [ -z $CURRENT_PID ] #2
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z "$CURRENT_PID" ]; then
+    echo "현재 실행 중인 애플리케이션이 없습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
-  sleep 5
+    echo "kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> $JAR_PATH 배포" #3
-nohup java -jar \
-        -Dspring.profiles.active=dev \
-        build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "새 애플리케이션 배포 시작"
+
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+
+echo "JAR Name: $JAR_NAME"
+
+# 백그라운드에서 Spring Boot 애플리케이션 실행
+# 로그 파일은 app.log에 기록
+nohup java -jar $REPOSITORY/build/libs/$JAR_NAME > $REPOSITORY/app.log 2>&1 &
+
+echo "새 애플리케이션 배포 완료"
