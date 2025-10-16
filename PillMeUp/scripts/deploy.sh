8645361e45c8fbb368a356731383ab5c0a759f20
mjkim <mjkim134@naver.com>
2025-10-16 13:21:35 +0900

    Feat: RDS 연결
    # Conflicts:
    #	.github/workflows/main.yml
    #	PillMeUp/scripts/deploy.sh

--- PillMeUp/scripts/deploy.sh
+++ PillMeUp/scripts/deploy.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/app
-CONFIG_FILE="$REPOSITORY/rds-config.env"
 PROJECT_NAME=pillmeup
-
-JAR_PATH="$REPOSITORY/PillMeUp/build/libs"
-JAR_NAME=$(ls -tr $JAR_PATH/ | grep 'SNAPSHOT.jar' | tail -n 1)
 
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
 
 echo "실행 중인 애플리케이션 PID: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "현재 실행 중인 애플리케이션이 없습니다. 새로 시작합니다."
+    echo "현재 실행 중인 애플리케이션이 없습니다."
 else
-    echo "기존 PID ${CURRENT_PID}를 종료합니다."
+    echo "kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
-    sleep 10
+    sleep 5
 fi
 
-if [ -f "$CONFIG_FILE" ]; then
-    echo "RDS 환경 변수 파일($CONFIG_FILE)을 로드합니다."
-    # source 명령으로 파일 내용을 현재 쉘의 환경 변수로 등록
-    source "$CONFIG_FILE"
-fi
+echo "새 애플리케이션 배포 시작"
 
-echo "새 애플리케이션 배포 시작: $JAR_NAME"
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 
-nohup java -jar \
-    -Dspring.profiles.active=prod \
-    -Dspring.datasource.url="$RDS_URL" \
-    -Dspring.datasource.username="$RDS_USERNAME" \
-    -Dspring.datasource.password="$RDS_PASSWORD" \
-    $JAR_PATH/$JAR_NAME > $REPOSITORY/app.log 2>&1 &
+echo "JAR Name: $JAR_NAME"
+
+# 백그라운드에서 Spring Boot 애플리케이션 실행
+# 로그 파일은 app.log에 기록
+nohup java -jar $REPOSITORY/build/libs/$JAR_NAME > $REPOSITORY/app.log 2>&1 &
 
 echo "새 애플리케이션 배포 완료"
