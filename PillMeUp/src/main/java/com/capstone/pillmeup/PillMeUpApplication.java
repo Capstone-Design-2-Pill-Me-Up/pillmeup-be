8ec244fba427dbbe3007ef3e52bed887767d4883
mjkim134 <mjkim134@naver.com>
2025-10-17 17:14:45 +0900

    fix

--- PillMeUp/src/main/java/com/capstone/pillmeup/PillMeUpApplication.java
+++ PillMeUp/src/main/java/com/capstone/pillmeup/PillMeUpApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class cdPillMeUpApplication {
+public class PillMeUpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PillMeUpApplication.class, args);
